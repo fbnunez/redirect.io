4249e878d7e89bce21f24fdbb0cd06694238cb46
fbNunez <43887329+fbnunez@users.noreply.github.com>
2020-08-13 22:46:49 -0300

    Better Schema error validation

--- backend/src/json_schema/UrlSchema.js
+++ backend/src/json_schema/UrlSchema.js
@@ -1,8 +1,11 @@
 const Joi = require('joi');
 const { urlRegex } = require('../helpers/urlRegex');
 
-const schema = Joi.object().keys({
+const urlValidation = Joi.object().keys({
   url: Joi.string().trim().pattern(urlRegex).required(),
+});
+
+const aliasValidation = Joi.object().keys({
   alias: Joi.string()
     .trim()
     .pattern(/[\w\-]/i)
@@ -11,9 +14,13 @@ const schema = Joi.object().keys({
 });
 
 const validate = async (body) => {
-  const validation = schema.validate(body);
-  if (validation.error) {
-    throw new Error(validation.error);
+  const urlIsValid = urlValidation.validate({ url: body.url });
+  if (urlIsValid.error) {
+    throw new Error('Your URL is not valid');
+  }
+  const aliasIsValid = aliasValidation.validate({ alias: body.alias });
+  if (aliasIsValid.error) {
+    throw new Error('Your ALIAS is not valid');
   }
   return true;
 };
